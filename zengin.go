bcf87f5bbfd0321135b941cdc9523a739a89e848
nametake <nametake.kyarabuki@gmail.com>
2018-01-26 01:55:22 +0900

    Add zengin.go

---
+++ zengin.go
@@ -0,0 +1,18 @@
+package zengin
+
+type Bank struct {
+	Code     string
+	Name     string
+	Kana     string
+	Hira     string
+	Roma     string
+	Branches map[string]Branch
+}
+
+type Branch struct {
+	Code string
+	Name string
+	Kana string
+	Hira string
+	Roma string
+}
